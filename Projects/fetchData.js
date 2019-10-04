94c634126f604ef90b74f9d79ade9e7b3bd79a72
Ethan <egruenemeier984@west-mec.org>
2019-10-04 14:58:42 -0700

    started pokemon search page
    updates api fetch based on user input.

--- Projects/fetchData.js
+++ Projects/fetchData.js
@@ -1,14 +1,22 @@
-fetch('https://pokeapi.co/api/v2/pokemon/5')
-.then( response => response.json() )
-.then( 
-    data => {
-        const {...pokemon} = data.sprites;
-        const {...type} = data.species;
-        console.log( pokemon );
-        console.log( type.name );
-        selector = document.querySelector('.pokemon');
-        selectorName = document.querySelector('.name');
-        selector.innerHTML = `<img height="500px" width="500px" src="${pokemon.front_shiny}" />`
-        selectorName.innerHTML = `<h3>Name: ${type.name} </h3> `
-    }
-);
+const pokeName = document.getElementById('userInput').value;
+fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+    .then(response => response.json())
+    .then(
+        data => {
+            const {
+                ...pokemon
+            } = data.sprites;
+            const {
+                ...type
+            } = data.species;
+            console.log(pokemon);
+            console.log(type.name);
+            selector = document.querySelector('.pokemon');
+            selectorName = document.querySelector('.name');
+            selector.innerHTML = `<img height="500px" width="500px" src="${pokemon.front_shiny}" />`
+            selectorName.innerHTML = `<h3>Name: ${type.name} </h3> `
+        }
+    );
+
+
+/// reload page
