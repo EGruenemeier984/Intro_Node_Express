74dde8562adb6280d6a12f12ba366f4face1aed8
Ethan <egruenemeier984@west-mec.org>
2019-10-09 15:22:34 -0700

    Created Template Engine
    Created a marko template page to display on the /home endpoint.

---
+++ Projects/index.marko.js
@@ -0,0 +1,53 @@
+// Compiled using marko@4.18.14 - DO NOT EDIT
+"use strict";
+
+var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/projects$1.0.0/index.marko",
+    components_helpers = require("marko/src/runtime/components/helpers"),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c,
+    marko_helpers = require("marko/src/runtime/html/helpers"),
+    marko_loadTag = marko_helpers.t,
+    lasso_head_tag = marko_loadTag(require("@lasso/marko-taglib/taglib/head-tag")),
+    component_globals_tag = marko_loadTag(require("marko/src/core-tags/components/component-globals-tag")),
+    lasso_body_tag = marko_loadTag(require("@lasso/marko-taglib/taglib/body-tag")),
+    init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer"));
+
+function render(input, out, __component, component, state) {
+  var data = input;
+
+  out.w("<html><head><title>HOME</title>");
+
+  lasso_head_tag({}, out, __component, "3");
+
+  out.w("</head><body>");
+
+  component_globals_tag({}, out);
+
+  lasso_body_tag({}, out, __component, "5");
+
+  init_components_tag({}, out);
+
+  await_reorderer_tag({}, out, __component, "6");
+
+  out.w("</body></html>");
+}
+
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
+
+marko_template.meta = {
+    id: "/projects$1.0.0/index.marko",
+    tags: [
+      "@lasso/marko-taglib/taglib/head-tag",
+      "marko/src/core-tags/components/component-globals-tag",
+      "@lasso/marko-taglib/taglib/body-tag",
+      "marko/src/core-tags/components/init-components-tag",
+      "marko/src/core-tags/core/await/reorderer-renderer"
+    ]
+  };
--- Projects/server.js
+++ Projects/server.js
@@ -1,16 +1,38 @@
+require("marko/node-require");
+
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+const port = 5000; 
+
 app.use(express.static('public'));
 
-app.post('/home', (req, res) => {
-	var query = req.body.userInput;
-	console.log(query);
-	res.render(query);
+var isProduction = process.env.NODE_ENV === 'production';
+
+// Configure lasso to control how JS/CSS/etc. is delivered to the browser
+require('lasso').configure({
+    plugins: [
+        'lasso-marko' // Allow Marko templates to be compiled and transported to the browser
+    ],
+    outputDir: __dirname + '/static', // Place all generated JS/CSS/etc. files into the "static" dir
+    bundlingEnabled: isProduction, // Only enable bundling in production
+    minify: isProduction, // Only minify JS and CSS code in production
+    fingerprintsEnabled: isProduction, // Only add fingerprints to URLs in production
+});
+
+app.use(require('lasso/middleware').serveStatic());
+
+app.use(express.urlencoded());
+
+app.post('/query-search', (req, res) => {
+    const query = req.body.userInput;
+    res.send(query);
+    res.end();
 });
 
 app.get('/home', (req, res) => {
-	res.sendFile(path.join(__dirname + '/index.html'));
+    res.setHeader("content-type", "text/html");
+    res.sendFile(path.join(__dirname + '/pages/index.marko'));
 });
 
 app.get('/script.js', (req, res) => {
@@ -22,14 +44,18 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(5000);
+app.listen(port, function() {
+    console.log('Server started! Try it out:\nhttp://localhost:' + port + '/');
+
+    if (process.send) {
+        process.send('online');
+    }
+});
 
 // get data from deezer api
 var unirest = require("unirest");
 
 var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
-
-// console.log(req.body)
 
 req.query({
 	"q": "logic"
