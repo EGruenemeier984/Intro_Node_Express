1f7598d23da3e8d99e2ae6bd877c279cfaef8f0f
Ethan <egruenemeier984@west-mec.org>
2019-10-16 15:00:02 -0700

    Changed template engine to EJS
    created partials and views using ejs plus updated the server.js to use ejs as the view.

--- Projects/server.js
+++ Projects/server.js
@@ -1,43 +1,25 @@
-require("marko/node-require").install();
-require("marko/express");
-
 const express = require('express');
-const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use('/public', express.static('public'));
 
-var templatePages = './src/pages';
-var indexTemplate = require(templatePages + '/index.marko');
-
-const people = [
-    {
-        name: "ethan",
-        age: 17
-    },
-    {
-        name: "justin ",
-        age: 17
-    }
-];
+app.set('view engine', 'ejs');
 
 app.get('/home', (req, res) => {
-    res.marko(indexTemplate, {
-        name: 'Ethan',
-        count: 100,
-        team: people
-    })
+    res.render('/views/pages/index.ejs');
 });
 
 app.get('/', (req, res) => {
     res.redirect('/home')
+}); 
+
+app.get('/about', (req, res) => {
+    res.render('/views/pages/about.ejs');
 });
 
 app.post('/query-search', (req, res) => {
-    const query = req.body.userInput;
-    res.send(query);
-    res.end();
+    res.write("HELLO");
 });
 
 app.listen(port, function () {
@@ -49,23 +31,24 @@ app.listen(port, function () {
 });
 
 // get data from deezer api
-var unirest = require("unirest");
+// var unirest = require("unirest");
 
-var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
+// const submitBtn = document.getElementById('submit');
+// submitBtn.addEventListener('click', async () => {
+//     var req = unirest("GET", "https://deezerdevs-deezer.p.rapidapi.com/search");
 
-req.query({
-    "q": "logic"
-});
+//     req.query({
+//         "q": "logic"
+//     });
 
-req.headers({
-    "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
-    "x-rapidapi-key": "2bce11ac03mshe8d42c0304556a9p1c94a3jsn42a650c97186"
-});
+//     req.headers({
+//         "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+//         "x-rapidapi-key": "2bce11ac03mshe8d42c0304556a9p1c94a3jsn42a650c97186"
+//     });
 
 
-req.end(function (res) {
-    if (res.error) throw new Error(res.error);
-
-    console.log(res.body);
-
-});
+//     req.end(function (res) {
+//         if (res.error) throw new Error(res.error);
+//         console.log(res.body);
+//     });
+// })
--- Projects/src/components/site-meta/README.md
+++
@@ -1,2 +0,0 @@
-# site-meta
-This component is a layout level component and should take in data and change the metatags according to page.
